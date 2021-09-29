f4edc2a2c144ff026eb2d29347f5145959f88c1c
JuandiM <juandimena34@gmail.com>
2021-09-29 13:04:22 +0200

    feat: starting routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer';
@@ -6,16 +8,16 @@ import HomeView from './views/HomeView';
 
 function App() {
   return (
-    <>
-    <Header />
-    <main className='py-3'>
-      <Container>
-        <HomeView />
-      </Container>
-     
-      </main>
-      <Footer />
-    </>
+    <Router>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <Route exact path='/' component={HomeView} />
+        </Container>
+      
+        </main>
+        <Footer />
+    </Router>
   );
 }
 
--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -10,7 +10,7 @@ const HomeView = () => {
         <>
             <h1>Products</h1>
             <Row>
-                {products.map(product =>(
+                {products.map(product =>( //Loop all products
                     <Col key={product._id} //Access to each product we are looping with the id
                         sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
