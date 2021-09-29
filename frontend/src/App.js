72b4cef1f09553ff970280220e4436ababcdda51
JuandiM <juandimena34@gmail.com>
2021-09-29 10:39:41 +0200

    react setup and starting frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+         <h1>Welcome to One-D Shop</h1>
+      </Container>
+     
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
