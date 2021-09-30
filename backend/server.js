8b03171f59500ab57c2c5afc7ce19a3bcaf17271
JuandiM <juandimena34@gmail.com>
2021-09-30 12:08:06 +0200

    feat: create model schema user, product & order

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,33 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
 
 dotenv.config()
 
+connectDB()
+
 const app = express ()
 
+//API GET ROUTES
 app.get ('/', (req, res) => {
     res.send ('API is working...')
 })
 
+//All products
 app.get('/api/products', (req, res) => {
     res.json(products)
 })
 
+//Each product by ID
 app.get('/api/products/:id', (req, res) => {
     const product = products.find(product => product._id === req.params.id)
     res.json(product)
 })
 
+//PORT CONFIG
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_EVN} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
