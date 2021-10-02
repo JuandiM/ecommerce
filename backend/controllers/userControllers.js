141e256d6fcd943e4ae1869123a226cadaf8d43a
JuandiM <juandimena34@gmail.com>
2021-10-02 17:44:19 +0200

    feat: backend update profile

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -71,5 +71,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 })
 
+// => UPDATE user profile => PUT api/users/profile (Private)
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
 
-export {authUser, getUserProfile, registerUser}
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        })
+
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+
+})
+
+
+export {authUser, getUserProfile, registerUser, updateUserProfile}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controllers/userControllers.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userControllers.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 
@@ -9,7 +9,9 @@ const router = express.Router()
 router.post('/login', authUser)
 
 //get user profile
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile')
+.get(protect, getUserProfile)
+.put(protect, updateUserProfile)
 
 //new user
 router.route('/').post(registerUser)
