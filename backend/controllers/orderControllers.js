fa71dd4d8a6729d87a7cb9da7d32bf7a2db9c1dd
JuandiM <juandimena34@gmail.com>
2021-10-03 18:18:56 +0200

    feat: backend working in orders

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -35,4 +35,37 @@ const addOrderItems = asyncHandler(async(req, res) => {
     }
 })
 
-export {addOrderItems}
+//Get an order by id-> GET /api/orders/:id - (private, protected)
+const getOrderById = asyncHandler(async(req, res) => {
+   const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+   if(order){
+       res.json(order)
+   } else {
+       res.status(404)
+       throw new Error ('Order not found')
+   }
+
+    if(orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Error ('No order items')
+        return
+    } else {
+        const order = new Order({
+        user: req.user._id,
+        orderItems, 
+        shippingAddress, 
+        paymentMethod, 
+        itemsPrice, 
+        taxPrice, 
+        shippingPrice, 
+        totalPrice
+        })
+
+        const createdOrder = await order.save()
+
+        res.status(201).json(createdOrder)
+    }
+})
+
+export {addOrderItems, getOrderById}
