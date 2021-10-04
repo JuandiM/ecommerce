5cc4d0262aab0d52a0e8d3be7a6450d539863f5b
JuandiM <juandimena34@gmail.com>
2021-10-04 10:04:30 +0200

    fix: permission error-users cannot see the userlist

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,6 +16,7 @@ USER_DETAILS_RESET,
 USER_LIST_REQUEST,
 USER_LIST_SUCCESS,
 USER_LIST_FAIL,
+USER_LIST_RESET
 } from '../constants/userConstants'
 
 import {ORDER_LIST_MY_RESET} from '../constants/orderConstants'
@@ -62,6 +63,7 @@ export const logout = () => (dispatch) => {
     dispatch({type: USER_LOGOUT})
     dispatch({type: USER_DETAILS_RESET})
     dispatch({type: ORDER_LIST_MY_RESET})
+    dispatch({type: USER_LIST_RESET})
 }
 
 //REGISTER
